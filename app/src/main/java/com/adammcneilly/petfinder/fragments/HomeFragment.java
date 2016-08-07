ac47417f4b4f6694a5a573032e973f5ae8864cf7
AdamMc331 <amcneilly331@gmail.com>
2016-08-07 03:06:06 -0400

    Added search by ID edit text.

--- app/src/main/java/com/adammcneilly/petfinder/fragments/HomeFragment.java
+++ app/src/main/java/com/adammcneilly/petfinder/fragments/HomeFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.AppCompatButton;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.adammcneilly.petfinder.R;
 import com.adammcneilly.petfinder.core.CoreFragment;
@@ -39,6 +42,15 @@ public class HomeFragment extends CoreFragment {
             @Override
             public void onClick(View view) {
                 showFragment(ScanFragment.FRAGMENT_NAME);
+            }
+        });
+
+        final EditText searchByIdEditText = (EditText) root.findViewById(R.id.search_by_id_edit_text);
+        searchByIdEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                showFragment(PetInfoFragment.FRAGMENT_NAME, searchByIdEditText.getText().toString());
+                return true;
             }
         });
 
